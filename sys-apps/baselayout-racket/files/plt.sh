23125c9603897a28c53ad49275321435f85ca4f5
Maciej Barć <xgqt@protonmail.com>
2021-02-17 04:56:41 +0100

    sys-apps/baselayout-racket: update PATH
    Signed-off-by: Maciej Barć <xgqt@protonmail.com>

--- sys-apps/baselayout-racket/files/plt.sh
+++ sys-apps/baselayout-racket/files/plt.sh
@@ -4,6 +4,10 @@
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# shellcheck disable=2044
+
+
+# Set PLTUSERHOME
 
 _pltuserhome="${EPREFIX}/usr/share/racket/gentoo/home"
 
@@ -14,3 +18,15 @@ then
 fi
 
 unset _pltuserhome
+
+
+# Add to PATH
+
+if [ -d "${PLTUSERHOME}/.racket" ]
+then
+    for i in $(find "${PLTUSERHOME}/.racket" -maxdepth 2 -name "bin" -type d)
+    do
+        export PATH="${PATH}:${i}"
+    done
+    unset i
+fi

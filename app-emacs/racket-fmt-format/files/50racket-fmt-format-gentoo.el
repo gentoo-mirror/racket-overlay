d2aa7842648a0789ce1f9f9a98e071bf0ed0f61d
Maciej Barć <xgqt@gentoo.org>
2022-07-25 04:59:29 +0200

    app-emacs/racket-fmt-format: new package; add version 2022.06.30
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ app-emacs/racket-fmt-format/files/50racket-fmt-format-gentoo.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'racket-fmt-format-buffer "racket-fmt-format" nil t)
+(autoload 'racket-fmt-format-region "racket-fmt-format" nil t)
+(autoload 'racket-fmt-format-on-save-mode "racket-fmt-format" nil t)
